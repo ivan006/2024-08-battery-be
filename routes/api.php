47c38fa1165d662a00f89365c1ad330a2335ec0d
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-08-07 21:07:16 +0200

    1

--- routes/api.php
+++ routes/api.php
@@ -19,7 +19,7 @@ Route::middleware('auth:sanctum')->group(function () {
     Route::get('/user', function (Request $request) {
         return $request->user();
     });
-    
+
 });
 
 Route::apiResource('posts', \App\Http\Controllers\Api\PostController::class);
