42ac4e6848dba1c6479139c8f54450ce6870e069
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-09-05 19:15:07 +0200

    1

--- database/migrations/2024_09_05_171346_create_location_suburbs_table.php
+++ database/migrations/2024_09_05_171346_create_location_suburbs_table.php
@@ -13,6 +13,7 @@ return new class extends Migration
     {
         Schema::create('location_suburbs', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
             $table->timestamps();
         });
     }
