8c33da26c98e714617f4d7ddadde48d86328b602
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-09-22 15:30:44 +0200

    1

--- database/migrations/2024_09_22_132817_create_private_events_table.php
+++ database/migrations/2024_09_22_132817_create_private_events_table.php
@@ -8,19 +8,32 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::create('private_events', function (Blueprint $table) {
             $table->id();
+            $table->unsignedInteger('family_id');
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->dateTime('start_datetime');
+            $table->dateTime('end_datetime');
+            $table->unsignedInteger('creator_id')->nullable();
             $table->timestamps();
+
+            $table->foreign('family_id')->references('id')->on('families')->onDelete('cascade');
+            $table->foreign('creator_id')->references('id')->on('users')->onDelete('set null');
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::dropIfExists('private_events');
     }
