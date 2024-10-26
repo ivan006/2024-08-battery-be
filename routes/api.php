fff05637037a516aa199fa9293ee10cfdb75c965
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-10-26 09:29:08 +0200

    1

--- routes/api.php
+++ routes/api.php
@@ -34,8 +34,8 @@ Route::apiResource('events', \App\Http\Controllers\Api\EventController::class);
 Route::apiResource('failed-jobs', \App\Http\Controllers\Api\FailedJobController::class);
 // API routes for families
 Route::apiResource('families', \App\Http\Controllers\Api\FamilyController::class);
-// API routes for family-ties
-Route::apiResource('family-ties', \App\Http\Controllers\Api\FamilyLinkController::class);
+// API routes for family-links
+Route::apiResource('family-links', \App\Http\Controllers\Api\FamilyLinkController::class);
 // API routes for jobs
 Route::apiResource('jobs', \App\Http\Controllers\Api\JobController::class);
 // API routes for migrations
