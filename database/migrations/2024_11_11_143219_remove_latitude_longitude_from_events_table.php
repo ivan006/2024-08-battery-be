607da3b1638010d3a8dbacd253d9bb4ab3549fdf
Ivan Copeland <ivan.copeland2015@gmail.com>
2024-11-11 16:33:55 +0200

    1

--- database/migrations/2024_11_11_143219_remove_latitude_longitude_from_events_table.php
+++ database/migrations/2024_11_11_143219_remove_latitude_longitude_from_events_table.php
@@ -8,21 +8,26 @@ return new class extends Migration
 {
     /**
      * Run the migrations.
+     *
+     * @return void
      */
-    public function up(): void
+    public function up()
     {
         Schema::table('events', function (Blueprint $table) {
-            //
+            $table->dropColumn(['latitude', 'longitude']);
         });
     }
 
     /**
      * Reverse the migrations.
+     *
+     * @return void
      */
-    public function down(): void
+    public function down()
     {
         Schema::table('events', function (Blueprint $table) {
-            //
+            $table->decimal('latitude', 10, 7)->nullable();
+            $table->decimal('longitude', 10, 7)->nullable();
         });
     }
 };
